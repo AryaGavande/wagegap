fdaa40245983a7b51df61598886b6e5e6e6db347
Arya <aryagavande@gmail.com>
2025-02-15 16:43:03 -0500

    updated frontend error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import Logo from '../assets/pizzaLogo.png';
+import Logo from '../assets/pizzaLogo.png'; // Correct import
 
 function Navbar() {
   return (
     <div className='navbar'>
-      <div className="leftSide"></div>
+      <div className="leftSide">
+        <img src={Logo} alt="Pizza Logo" /> {/* Use imported Logo variable */}
+      </div>
       <div className="rightSide"></div>
-        <img src={Logo}/>
     </div>
   );
 }
 
-export default Navbar
+export default Navbar;
