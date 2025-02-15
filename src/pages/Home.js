c40c8bceb48220522da68dd01e1ee57bad5e5da0
likithat18 <likitha@unc.edu>
2025-02-15 18:10:08 -0500

    Navbar made

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div> Hello World!</div>
+    <div className="home">
+        <div className="headerContainer">
+            <h1> Gals's Pizzaeria</h1>
+            <p> All GALS CAN DO THIS</p>
+            <Link to="/menu">
+            <button> TRY NOW </button>
+            </Link>
+        </div>
+    </div>
   )
 }
 
